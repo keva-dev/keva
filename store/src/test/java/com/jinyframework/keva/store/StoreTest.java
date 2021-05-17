ba14aed731621b2433b13dddb1ff3170141dd77f
Tu Huynh <tu.huynh@linecorp.com>
2021-05-18 05:22:18 +0700

    update fix bug

--- store/src/test/java/com/jinyframework/keva/store/StoreTest.java
+++ store/src/test/java/com/jinyframework/keva/store/StoreTest.java
@@ -1,15 +1,15 @@
 package com.jinyframework.keva.store;
 
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import lombok.*;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 public class StoreTest {
     private static final String storeName = "junit-test";
     private static NoHeapStoreManager manager = null;
@@ -17,20 +17,36 @@ public class StoreTest {
 
     @BeforeAll
     static void initStore() {
-        manager = new NoHeapStoreManager();
-        manager.createStore(storeName, NoHeapStore.Storage.IN_MEMORY, 1843);
-        noHeapStore = manager.getStore(storeName);
+        try {
+            manager = new NoHeapStoreManager();
+            manager.createStore(storeName, NoHeapStore.Storage.IN_MEMORY, 8);
+            noHeapStore = manager.getStore(storeName);
+        } catch (Exception e) {
+            fail(e);
+        }
     }
 
     @Test
     void getSet() {
         val setAbc = noHeapStore.putString("abc", "123");
         val getAbc = noHeapStore.getString("abc");
-        val getNull = noHeapStore.getString("notExistedKey");
+        val getNull = noHeapStore.getString("notExisted");
         assertTrue(setAbc);
         assertTrue("123".contentEquals(getAbc));
-        assertTrue("null".contentEquals(getNull));
+        assertNull(getNull);
     }
+
+    @Test
+    void reAssign() {
+        noHeapStore.putString("key1", "val1");
+        assertTrue("val1".contentEquals(noHeapStore.getString("key1")));
+        noHeapStore.putString("key1", "val2");
+        assertTrue("val2".contentEquals(noHeapStore.getString("key1")));
+        noHeapStore.remove("key1");
+        assertNull(noHeapStore.getString("key1"));
+    }
+
+    // TODO: Add collision cases
 
     @AfterAll
     static void clean() {
