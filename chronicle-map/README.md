90d4ef3f5c2b9b6527a44b04d24700662dd509ca
Tu Huynh <tu.huynh@linecorp.com>
2021-05-22 04:53:48 +0700

    add chronicle demo

---
+++ chronicle-map/README.md
@@ -0,0 +1,8 @@
+# Chronicle Map demo
+
+Run:
+```shell
+git clone git@github.com:tuhuynh27/keva.git
+cd keva/chronicle-map
+./demo
+```
---
+++ chronicle-map/src/main/java/com/jinyframework/keva/chronicle/Main.java
@@ -0,0 +1,25 @@
+package com.jinyframework.keva.chronicle;
+
+import net.openhft.chronicle.map.ChronicleMap;
+import net.openhft.chronicle.map.ChronicleMapBuilder;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+class Main {
+    public static void main(String[] args) {
+        ChronicleMap<String, String> chronicleMap = ChronicleMapBuilder
+                .of(String.class, String.class)
+                .name("demo")
+                .entries(100).averageKey("this-is-test-key").averageValue("this-is-average-val")
+                .create();
+        chronicleMap.put("test", "Hello World");
+
+        log.info("Test is: " + chronicleMap.get("test"));
+        log.info("Undefined is: " + chronicleMap.get("test1"));
+
+        chronicleMap.remove("test");
+
+        log.info("Test is: " + chronicleMap.get("test"));
+    }
+}
