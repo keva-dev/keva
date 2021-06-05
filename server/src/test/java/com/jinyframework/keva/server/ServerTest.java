9dd4a0acb78138ceeac040743b34368b9da5ef11
Nguyen Quoc Viet <nguyenqviet98@gmail.com>
2021-06-05 19:40:47 +0700

    Refactor: add server tests (#13)
    * add multi client - 1 socket test
    
    * add server tests with long values
    
    Co-authored-by: Tu Huynh <13906546+tuhuynh27@users.noreply.github.com>

--- server/src/test/java/com/jinyframework/keva/server/ServerTest.java
+++ server/src/test/java/com/jinyframework/keva/server/ServerTest.java
@@ -8,9 +8,15 @@ import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 import java.io.File;
-import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 public class ServerTest extends AbstractServerTest {
@@ -34,11 +40,11 @@ public class ServerTest extends AbstractServerTest {
         deleteFile("./KevaDataIndex");
 
         server = new Server(ConfigHolder.defaultBuilder()
-                // TODO: check why adding snapshotEnabled = false make test fail
-                .snapshotEnabled(false)
-                .hostname(host)
-                .port(port)
-                .build());
+                                        // TODO: check why adding snapshotEnabled = false make test fail
+                                        .snapshotEnabled(false)
+                                        .hostname(host)
+                                        .port(port)
+                                        .build());
         new Thread(() -> {
             try {
                 server.run();
@@ -56,8 +62,109 @@ public class ServerTest extends AbstractServerTest {
     }
 
     @AfterAll
-    static void stop() throws Exception {
+    static void stop() {
         client.disconnect();
         server.shutdown();
+    }
+
+    @Test
+    void multiClientGet() {
+        try {
+            val setAbc = client.exchange("set abc 123");
+            assertEquals("1", setAbc);
+        } catch (Exception e) {
+            fail(e);
+        }
+
+        final List<Callable<Object>> tasks = new ArrayList<>();
+        final ExecutorService executor = Executors.newFixedThreadPool(2);
+        final int taskNum = 2;
+        for (int i = 0; i < taskNum; i++) {
+            final int finalI = i;
+            tasks.add(() ->
+                      {
+                          System.out.println("task: " + finalI);
+                          return client.exchange("get abc");
+                      });
+        }
+        try {
+            final List<Future<Object>> futures = executor.invokeAll(tasks, 2, TimeUnit.SECONDS);
+            assertFalse(futures.isEmpty());
+            assertEquals(taskNum, futures.size());
+            for (Future<Object> future : futures) {
+                assertEquals("123", future.get().toString());
+            }
+
+            System.out.println(futures.size());
+        } catch (Exception e) {
+            fail(e);
+        }
+    }
+
+    @Test
+    @Timeout(5)
+    void getSetLongString() {
+        try {
+            final int aKB = 1024;
+            String testStr = "a".repeat(Math.max(0, aKB));
+            String setAbc = client.exchange("set abc " + testStr);
+            String getAbc = client.exchange("get abc");
+            assertEquals("1", setAbc);
+            assertEquals(testStr, getAbc);
+
+            final int aMB = 1024 * aKB;
+            testStr = "a".repeat(Math.max(0, aMB));
+            setAbc = client.exchange("set abc " + testStr);
+            getAbc = client.exchange("get abc");
+            assertEquals("1", setAbc);
+            assertEquals(testStr, getAbc);
+
+            final int MB4 = 4 * aMB;
+            testStr = "a".repeat(Math.max(0, MB4));
+            setAbc = client.exchange("set abc " + testStr);
+            getAbc = client.exchange("get abc");
+            assertEquals("1", setAbc);
+            assertEquals(testStr, getAbc);
+        } catch (Exception e) {
+            fail(e);
+        }
+    }
+
+    @Test
+    @Timeout(20)
+    void getSetLongerString() {
+        try {
+            final int aMB = 1024 * 1024;
+            final int MB8 = 8 * aMB;
+            String testStr = "a".repeat(Math.max(0, MB8));
+            String setAbc = client.exchange("set abc " + testStr);
+            String getAbc = client.exchange("get abc");
+            assertEquals("1", setAbc);
+            assertEquals(testStr, getAbc);
+
+            final int MB16 = 16 * aMB;
+            testStr = "a".repeat(Math.max(0, MB16));
+            setAbc = client.exchange("set abc " + testStr);
+            getAbc = client.exchange("get abc");
+            assertEquals("1", setAbc);
+            assertEquals(testStr, getAbc);
+        } catch (Exception e) {
+            fail(e);
+        }
+    }
+
+    @Test
+    @Timeout(5)
+    void getSetLongKeyString() {
+        try {
+            final int aKB = 1023;
+            String testStr = "a".repeat(Math.max(0, aKB));
+            String setAbc = client.exchange("set " + testStr + " 123");
+            String getAbc = client.exchange("get " + testStr);
+            assertEquals("1", setAbc);
+            assertEquals("123", getAbc);
+        } catch (Exception e) {
+            fail(e);
+        }
     }
 }
