e45f1ae1af4a1186b23ef9536889ad21a8795eb7
Huynh, Tu | RASIA <tu.huynh@rakuten.com>
2021-10-28 19:00:25 +0800

    remove unused code

--- server/src/main/java/dev/keva/server/command/setup/CommandService.java
+++ server/src/main/java/dev/keva/server/command/setup/CommandService.java
@@ -3,13 +3,6 @@ package dev.keva.server.command.setup;
 import dev.keva.server.protocol.redis.Command;
 import dev.keva.server.protocol.redis.Reply;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public interface CommandService {
-    static ArrayList<String> parseTokens(String line) {
-        return new ArrayList<>(Arrays.asList(line.split(" ")));
-    }
-
     Reply<?> handleCommand(String name, Command command);
 }
