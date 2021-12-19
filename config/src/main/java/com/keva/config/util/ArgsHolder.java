b6b0ae92b1f496f1187c4668cb46c315ce02d63b
Tu Huynh <tu.huynh@rakuten.com>
2021-12-19 23:28:08 +0800

    fix keva config (#88)

--- config/src/main/java/com/keva/config/util/ArgsHolder.java
+++ config/src/main/java/com/keva/config/util/ArgsHolder.java
@@ -19,7 +19,7 @@ public class ArgsHolder {
     @SuppressWarnings("ReturnOfNull")
     public String getFlag(String name) {
         if (values.containsKey(name)) {
-            return "true".equalsIgnoreCase(name) ? "true" : "false";
+            return "true".equalsIgnoreCase(values.get(name)) ? "true" : "false";
         }
         if (flags.contains(name)) {
             return "true";
