d4374bd27f70852cb1ff38b9952905e90d2dd09a
Huynh, Tu | RASIA <tu.huynh@rakuten.com>
2021-12-23 18:50:53 +0800

    done for server package

--- core/src/main/java/dev/keva/core/server/NettyChannelHandler.java
+++ core/src/main/java/dev/keva/core/server/NettyChannelHandler.java
@@ -1,6 +1,7 @@
 package dev.keva.core.server;
 
 import dev.keva.core.command.mapping.CommandMapper;
+import dev.keva.core.command.mapping.CommandWrapper;
 import dev.keva.ioc.annotation.Autowired;
 import dev.keva.ioc.annotation.Component;
 import dev.keva.protocol.resp.Command;
@@ -12,7 +13,6 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.timeout.IdleStateEvent;
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
 
 @Slf4j
 @Sharable
@@ -27,8 +27,8 @@ public class NettyChannelHandler extends SimpleChannelInboundHandler<Command> {
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, Command command) throws InterruptedException {
-        val name = command.getName();
-        val commandWrapper = commandMapper.getMethods().get(new BytesKey(name));
+        byte[] name = command.getName();
+        CommandWrapper commandWrapper = commandMapper.getMethods().get(new BytesKey(name));
         Reply<?> reply;
         if (commandWrapper == null) {
             reply = new ErrorReply("ERR unknown command `" + new String(name) + "`");
