abf75b12276792cd6abe6bb84a260e0fd6b07c77
ndaadn212 <nguyendinhan97@gmail.com>
2022-05-07 11:54:30 +0900

    more string cmds

---
+++ core/src/main/java/dev/keva/core/command/impl/string/SetNX.java
@@ -0,0 +1,37 @@
+package dev.keva.core.command.impl.string;
+
+import dev.keva.core.command.annotation.CommandImpl;
+import dev.keva.core.command.annotation.Execute;
+import dev.keva.core.command.annotation.ParamLength;
+import dev.keva.core.exception.CommandException;
+import dev.keva.ioc.annotation.Autowired;
+import dev.keva.ioc.annotation.Component;
+import dev.keva.protocol.resp.reply.IntegerReply;
+import dev.keva.store.KevaDatabase;
+
+import java.util.Arrays;
+
+import static dev.keva.core.command.annotation.ParamLength.Type.AT_LEAST;
+import static dev.keva.core.command.annotation.ParamLength.Type.EXACT;
+
+@Component
+@CommandImpl("setnx")
+@ParamLength(type = EXACT, value = 2)
+public class SetNX {
+    private final KevaDatabase database;
+
+    @Autowired
+    public SetNX(KevaDatabase database) {
+        this.database = database;
+    }
+
+    @Execute
+    public IntegerReply execute(byte[]... params) {
+        byte[] get = database.get(params[0]);
+        if(get != null) {
+            return new IntegerReply(0);
+        }
+        database.put(params[0], params[1]);
+        return new IntegerReply(1);
+    }
+}
