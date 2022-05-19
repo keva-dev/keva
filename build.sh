ebe9f287ad6aab546097f491c7d1e57bb463820f
Tu Huynh <tu.huynh@rakuten.com>
2022-05-19 10:21:07 +0800

    update release flow docs (#133)

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ./gradlew clean :app:fatJar -x test
-cat ./compile.sh ./app/build/libs/app-1.0-SNAPSHOT-all.jar > ./dist/unix/keva-server
+cat ./compile.sh ./app/build/libs/*-all.jar > ./dist/unix/keva-server
 echo "Done"
